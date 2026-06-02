4efd19e005f32ae7ec3b6879d48af615467bd9f3
talharizwan166-code <talharizwan166@gmail.com>
2026-06-02 11:01:15 +0500

    update blog.main.py

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,35 @@
-from fastapi import FastAPI
-from  . import schemas, models
-from  .database import engine
+from fastapi import FastAPI,Depends,status 
+from  . import schemas
+from  . import models
+from  .database import SessionLocal,engine 
+from sqlalchemy.orm import Session
+
 app=FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-@app.post('/blog')
-def create(requst:schemas.blog):
-    return requst 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED) 
+def create(request: schemas.Blog, db : Session=Depends(get_db)):
+
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def all(db : Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def show(id, db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
-class blog(BaseModel):
-    tittle: str
-    body: str
+class Blog(BaseModel):
+    title: str
+    body: str 
+    
