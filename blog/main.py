549f99ca1d596d24c37114346c18708018fe2185
talharizwan166-code <talharizwan166@gmail.com>
2026-06-09 12:45:48 +0500

    updated all files with new version

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI,Depends,status 
+from typing import List
+from fastapi import FastAPI,Depends,status,Response, HTTPException
 from  . import schemas
 from  . import models
 from  .database import SessionLocal,engine 
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
 app=FastAPI()
 
@@ -24,12 +26,49 @@ def create(request: schemas.Blog, db : Session=Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} not found')
+    
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'  
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} not found')
+    blog.update(request.model_dump())
+    db.commit()
+    return 'updated'
+    
+
+@app.get('/blog', status_code=200,response_model=list[schemas.ShowBlog])
 def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def show(id, db : Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=200,response_model=schemas.ShowBlog)
+def show(id, response: Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} not found')
+ 
+    
     return blog
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+@app.post('/user')
+def create_user(request: schemas.User, db : Session = Depends(get_db)):
+    hashedpassword = pwd_context.hash(request.password)
+    new_user = models.User(name = request.name, email = request.email, password = hashedpassword)
+    db.add(new_user)
+    db.commit() 
+    db.refresh(new_user)    
+    return new_user
+
--- blog/schemas.py
+++ blog/schemas.py
@@ -4,3 +4,18 @@ class Blog(BaseModel):
     title: str
     body: str 
     
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+
+
+    class Config():
+        orm_mode = True
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+    
